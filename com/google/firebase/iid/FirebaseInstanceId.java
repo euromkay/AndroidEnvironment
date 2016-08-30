04c5bc909125eb42dc15ad1a2c983594736e26c9
michaelcmkay <euromkay@gmail.com>
2016-08-29 20:51:24 -0700

    Token Class add

---
+++ com/google/firebase/iid/FirebaseInstanceId.java
@@ -0,0 +1,13 @@
+package com.google.firebase.iid;
+
+public class FirebaseInstanceId {
+
+	public static FirebaseInstanceId getInstance() {
+		return new FirebaseInstanceId();
+	}
+
+	public String getToken() {
+		return "";
+	}
+	
+}
