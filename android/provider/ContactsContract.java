1ab0f4ae26d51c4b94b6e3757fa23d3fcdedcd46
michaelcmkay <euromkay@gmail.com>
2016-09-17 18:44:43 -0700

    Import fix

--- android/provider/ContactsContract.java
+++ android/provider/ContactsContract.java
@@ -8,22 +8,22 @@ public class ContactsContract {
 	public static class CommonDataKinds{
 		
 		
-		public static final Phone Phone = new Phone();
-		public static Contactables Contactables = new Contactables();
+		public final Phone Phone = new Phone();
+		public Contactables Contactables = new Contactables();
 		public static class Contactables{
 
-			public static final int CONTENT_FILTER_URI = 0;
-			public static final int HAS_PHONE_NUMBER = 1;
-			public static final String LOOKUP_KEY = "";
-			public static final int DISPLAY_NAME = 0;
-			public static final int MIMETYPE = 0;
+			public final int CONTENT_FILTER_URI = 0;
+			public final int HAS_PHONE_NUMBER = 1;
+			public final String LOOKUP_KEY = "";
+			public final int DISPLAY_NAME = 0;
+			public final int MIMETYPE = 0;
 			
 		}
 		
 		public static class Phone{
 
-			public static final int NUMBER = 0;
-			public static final int CONTENT_ITEM_TYPE = 0;
+			public final int NUMBER = 0;
+			public final int CONTENT_ITEM_TYPE = 0;
 			
 		}
 		
